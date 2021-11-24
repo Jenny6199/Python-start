08babc110ac98e371fd5ed78dfcb00408c462f27
Jenny6199 <Jenny6199@yandex.ru>
2021-11-25 01:06:39 +0300

    complite_task_1

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -1,1 +1,24 @@
-task_1
+"""
+Выполнение практических заданий к уроку №1 Python-start.
+Студент: Максим Сапунов Jenny6199@yandex.ru
+
+Задача №1. Поработайте с переменными, создайте несколько,
+выведите на экран, запросите у пользователя несколько чисел и строк и
+сохраните в переменные, выведите их на экран.
+"""
+name = 'Maxim'
+surname = 'Sapunov'
+age = 37
+gender = 'male'
+position = 1
+
+duration = input('Как долго вы в IT? :')
+city = input('Из какого Вы города? :')
+
+print('name: ', name)
+print('surname: ', surname)
+print('age: ', age)
+print('gender: ', gender)
+print('position: ', position)
+print('duration: ', duration)
+print('city: ', city)
